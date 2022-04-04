06e2215ccf6d44fb1a90d3933d36c1c1911a8438
Milad Jalali <54532218+miladjalali1364@users.noreply.github.com>
2022-04-04 11:14:49 +0430

    read csv file and print first 5 row

--- Model-Gensim.py
+++ Model-Gensim.py
@@ -1,3 +1,8 @@
 
 import pandas as pd
 from gensim.models import word2vec
+
+data_frame = pd.read_csv("data.csv",index_col=False)
+print(data_frame.head(5))
+
+data_frame['Maker_Model'] = data_frame['Make'] + ' ' + data_frame['Model']
