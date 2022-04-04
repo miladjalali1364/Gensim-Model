fd8397359699b160b264da921c695404f8d7a04d
Milad Jalali <54532218+miladjalali1364@users.noreply.github.com>
2022-04-04 11:24:15 +0430

    Select feature from original dataset to form a new dataframe

--- Model-Gensim.py
+++ Model-Gensim.py
@@ -6,3 +6,8 @@ data_frame = pd.read_csv("data.csv",index_col=False)
 print(data_frame.head(5))
 
 data_frame['Maker_Model'] = data_frame['Make'] + ' ' + data_frame['Model']
+
+# Select features from original dataset to form a new dataframe
+data_frame1 = data_frame[['Engine Fuel Type','Transmission Type', 'Driven_Wheels',
+                         'Market Category', 'Vehicle Size', 'Vehicle Style','Maker_Model']]
+
