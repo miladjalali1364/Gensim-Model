eb7fe0ce2d328b2b59128cd687a7a4b251337de0
Milad Jalali <54532218+miladjalali1364@users.noreply.github.com>
2022-04-04 11:30:49 +0430

    combine all columns into one column

--- Model-Gensim.py
+++ Model-Gensim.py
@@ -11,3 +11,5 @@ data_frame['Maker_Model'] = data_frame['Make'] + ' ' + data_frame['Model']
 data_frame1 = data_frame[['Engine Fuel Type','Transmission Type', 'Driven_Wheels',
                          'Market Category', 'Vehicle Size', 'Vehicle Style','Maker_Model']]
 
+# For each row, combine all the columns into one column
+df2 = data_frame1.apply(lambda x: ','.join(x.astype(str)), axis=1)
